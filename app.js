e760bf7a1da7deba6afc6843beee3c52feb4b5f7
Morgan Herlocker <morgan.herlocker@gmail.com>
2012-11-05 14:03:19 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , socket = require('socket.io');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io = socket.listen(server), {log:false};
+io.set('log level', 1);
+
+io.sockets.on('connection', function(socket){
+  socket.on('newMessage', function(data){
+    io.sockets.emit('messageUpdate', data);
+  });
+});
+
